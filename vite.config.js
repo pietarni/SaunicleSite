2b4109d29123991bfdd28512fa4bf17591a2ce8e
pietarni <pietari.niinimaki@helsinki.fi>
2025-03-08 11:05:27 +0200

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // or vue/svelte, depending on your app
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SaunicleSite/', // <-- Set this to your GitHub repo name
-});
+  base: '/', // keep this as '/' since you're using a custom domain
+})
