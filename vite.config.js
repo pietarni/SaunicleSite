91f8fdcf06b8f70bcca6d0e4df46d7ac67a8a05d
pietarni <pietari.niinimaki@helsinki.fi>
2025-03-08 11:06:46 +0200

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or vue/svelte, depending on your app
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
+  base: '', 
   plugins: [react()],
-  base: '/', // keep this as '/' since you're using a custom domain
 })
